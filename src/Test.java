fb8511d5166717228272c7d2d39aa0b753a409be
s11ngh <ujjwalwrk@gmail.com>
2022-06-28 04:21:10 +0530

    first push

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import knox.sudoku.Sudoku;
+
+public class Test {
+    public static void main(String[] args) {
+        Sudoku sudoku = new Sudoku();
+        sudoku.load("/Users/ujjwalsingh/Downloads/cs220-sudoku-main/easy1.txt");
+        System.out.println(sudoku);
+
+        System.out.println(sudoku.getLegalValues(6,8));
+    }
+}
---
+++ src/knox/sudoku/Sudoku.java
@@ -0,0 +1,134 @@
+package knox.sudoku;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * 
+ * This is the MODEL class. This class knows all about the
+ * underlying state of the Sudoku game. We can VIEW the data
+ * stored in this class in a variety of ways, for example,
+ * using a simple toString() method, or using a more complex
+ * GUI (Graphical User Interface) such as the SudokuGUI 
+ * class that is included.
+ * 
+ * @author jaimespacco
+ *
+ */
+public class Sudoku {
+	int[][] board = new int[9][9];
+	
+	public int get(int row, int col) {
+		// TODO: check for out of bounds
+		return board[row][col];
+	}
+	
+	public void set(int row, int col, int val) {
+		// TODO: make sure val is legal
+		board[row][col] = val;
+	}
+	
+	public boolean isLegal(int row, int col, int val) {
+		// TODO: check if it's legal to put val at row, col
+		return true;
+	}
+	
+	public Collection<Integer> getLegalValues(int row, int col) {
+		// TODO: return only the legal values that can be stored at the given row, col
+		Set<Integer> result = new HashSet<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+		for(int i=0; i<9;i++){
+			result.remove(board[row][i]);
+			result.remove(board[i][col]);
+		}
+		int rstart = row/3*3;
+		int cstart = col/3*3;
+		for(int r=rstart; r<rstart+3;r++){
+			for(int c=cstart; c<cstart+3;c++){
+				result.remove(board[r][c]);
+			}
+		}
+		return  result;
+	}
+	
+/**
+
+_ _ _ 3 _ 4 _ 8 9
+1 _ 3 2 _ _ _ _ _
+etc
+
+
+0 0 0 3 0 4 0 8 9
+
+ */
+	public void load(String filename) {
+		try {
+			Scanner scan = new Scanner(new FileInputStream(filename));
+			// read the file
+			for (int r=0; r<9; r++) {
+				for (int c=0; c<9; c++) {
+					int val = scan.nextInt();
+					board[r][c] = val;
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Return which 3x3 grid this row is contained in.
+	 * 
+	 * @param row
+	 * @return
+	 */
+	public int get3x3row(int row) {
+		return row / 3;
+	}
+	
+	/**
+	 * Convert this Sudoku board into a String
+	 */
+	public String toString() {
+		String result = "";
+		for (int r=0; r<9; r++) {
+			for (int c=0; c<9; c++) {
+				int val = get(r, c);
+				if (val == 0) {
+					result += "_ ";
+				} else {
+					result += val + " ";
+				}
+			}
+			result += "\n";
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Sudoku sudoku = new Sudoku();
+		sudoku.load("easy1.txt");
+		System.out.println(sudoku);
+		
+		Scanner scan = new Scanner(System.in);
+		while (!sudoku.gameOver()) {
+			System.out.println("enter value r, c, v :");
+			int r = scan.nextInt();
+			int c = scan.nextInt();
+			int v = scan.nextInt();
+			sudoku.set(r, c, v);
+
+			System.out.println(sudoku);
+		}
+	}
+
+	public boolean gameOver() {
+		// TODO check that there are still open spots
+		return false;
+	}
+
+	public boolean isBlank(int row, int col) {
+		return board[row][col] == 0;
+	}
+
+}
